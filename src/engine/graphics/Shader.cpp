033450289d1380252612293ffdb24bcfe3590f76
gruco0002 <dev.gruco0002@icloud.com>
2019-04-17 19:02:56 +0200

    Shader

---
+++ src/engine/graphics/Shader.cpp
@@ -0,0 +1,177 @@
+//
+// Created by corbi on 17.04.2019.
+//
+
+#include <engine/EngineException.hpp>
+#include "Shader.hpp"
+#include <libraries/glm/gtc/type_ptr.hpp>
+
+uint32_t  Engine::Graphics::Shader::currentlyBound = 0;
+
+Engine::Graphics::Shader::Shader(std::vector<Engine::Graphics::Shader::ProgramPart> parts,
+                                 std::vector<std::string> defines) : parts(parts), defines(defines) {
+    Generate();
+}
+
+Engine::Graphics::Shader::Shader(std::vector<Engine::Graphics::Shader::ProgramPart> parts) : parts(parts) {
+    Generate();
+}
+
+Engine::Graphics::Shader::~Shader() {
+    Delete();
+}
+
+void Engine::Graphics::Shader::Bind() {
+    if (currentlyBound == ID)
+        return;
+    glUseProgram(ID);
+    currentlyBound = ID;
+}
+
+void Engine::Graphics::Shader::Unbind() {
+    glUseProgram(0);
+    currentlyBound = 0;
+}
+
+void Engine::Graphics::Shader::SetValue(const std::string &name, bool value) {
+    Bind();
+    glUniform1i(GetUniformLoaction(name), (int32_t) value);
+}
+
+void Engine::Graphics::Shader::SetValue(const std::string &name, int32_t value) {
+    Bind();
+    glUniform1i(GetUniformLoaction(name), value);
+}
+
+void Engine::Graphics::Shader::SetValue(const std::string &name, float_t value) {
+    Bind();
+    glUniform1f(GetUniformLoaction(name), value);
+}
+
+void Engine::Graphics::Shader::SetValue(const std::string &name, glm::vec2 value) {
+    Bind();
+    glUniform2f(GetUniformLoaction(name), value.x, value.y);
+}
+
+void Engine::Graphics::Shader::SetValue(const std::string &name, glm::vec3 value) {
+    Bind();
+    glUniform3f(GetUniformLoaction(name), value.x, value.y, value.z);
+}
+
+void Engine::Graphics::Shader::SetValue(const std::string &name, glm::vec4 value) {
+    Bind();
+    glUniform4f(GetUniformLoaction(name), value.x, value.y, value.z, value.w);
+}
+
+void Engine::Graphics::Shader::SetValue(const std::string &name, glm::mat3 value) {
+    Bind();
+    glUniformMatrix3fv(GetUniformLoaction(name), 1, GL_FALSE, glm::value_ptr(value));
+}
+
+void Engine::Graphics::Shader::SetValue(const std::string &name, glm::mat4 value) {
+    Bind();
+    glUniformMatrix4fv(GetUniformLoaction(name), 1, GL_FALSE, glm::value_ptr(value));
+}
+
+void Engine::Graphics::Shader::SetValue(const std::string &name, Engine::Graphics::Texture2D *texture, uint32_t unit) {
+    Bind();
+    texture->Bind(unit);
+    glUniform1i(GetUniformLoaction(name), unit);
+}
+
+GLint Engine::Graphics::Shader::GetUniformLoaction(const std::string &name) {
+    if (foundNames.find(name) != foundNames.end())
+        return uniformLoactions[name];
+
+    GLint location = glGetUniformLocation(ID, name.c_str());
+    if (location == -1)
+        throw EngineException("Shader Error: Cannot retrieve uniform variable with this name!");
+
+    foundNames.insert(name);
+    uniformLoactions[name] = location;
+    return location;
+}
+
+void Engine::Graphics::Shader::Generate() {
+
+    // set defines
+    InsertDefinesIntoCode();
+
+    // compile shader parts
+    std::vector<uint32_t> compiledShaders;
+    for (ProgramPart &part: parts) {
+        compiledShaders.push_back(CompileShader(part));
+    }
+
+    // link shader parts
+    ID = glCreateProgram();
+    for (uint32_t compiledShader: compiledShaders) {
+        glAttachShader(ID, compiledShader);
+    }
+    glLinkProgram(ID);
+
+    // check for linking errors
+    GLint isCompiled = 0;
+    glGetProgramiv(ID, GL_LINK_STATUS, &isCompiled);
+    if (isCompiled == GL_FALSE) {
+
+        GLint maxLength = 0;
+        glGetProgramiv(ID, GL_INFO_LOG_LENGTH, &maxLength);
+        std::vector<GLchar> errorLog(maxLength);
+        glGetProgramInfoLog(ID, maxLength, &maxLength, &errorLog[0]);
+        throw EngineException(
+                ("Shader Error: Error linking the shader program parts together! " +
+                 std::string(errorLog.data())).c_str());
+    }
+
+    // delete program parts
+    for (uint32_t compiledShader: compiledShaders) {
+        glDetachShader(ID, compiledShader);
+        glDeleteShader(compiledShader);
+    }
+
+
+}
+
+void Engine::Graphics::Shader::Delete() {
+    glDeleteProgram(ID);
+}
+
+uint32_t Engine::Graphics::Shader::CompileShader(Engine::Graphics::Shader::ProgramPart &part) {
+    uint32_t shaderID = glCreateShader(part.type);
+    auto source = part.code.c_str();
+    glShaderSource(shaderID, 1, &source, nullptr);
+    glCompileShader(shaderID);
+
+    // check for compiling errors
+    GLint isCompiled = 0;
+    glGetShaderiv(shaderID, GL_COMPILE_STATUS, &isCompiled);
+    if (isCompiled == GL_FALSE) {
+
+        GLint maxLength = 0;
+        glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &maxLength);
+        std::vector<GLchar> errorLog(maxLength);
+        glGetShaderInfoLog(shaderID, maxLength, &maxLength, &errorLog[0]);
+
+        throw EngineException(
+                ("Shader Error: Error compiling the shader program part! " + std::string(errorLog.data())).c_str());
+    }
+
+    return shaderID;
+}
+
+void Engine::Graphics::Shader::InsertDefinesIntoCode() {
+    std::reverse(defines.begin(),
+                 defines.end()); // reverse the defines, since they are added in reverse order => End result is adding in correct order
+
+    for (ProgramPart &part: parts) {
+
+        // find first newline
+        auto firstNewLine = part.code.find('\n');
+
+        // insert defines after that line
+        for (std::string &def : defines) {
+            part.code.insert(firstNewLine + 1, "#define " + def + "\n");
+        }
+    }
+}
---
+++ src/engine/graphics/Shader.hpp
@@ -0,0 +1,96 @@
+//
+// Created by corbi on 17.04.2019.
+//
+
+#ifndef FLUIDSOLVER_SHADER_HPP
+#define FLUIDSOLVER_SHADER_HPP
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <libraries/glm/glm.hpp>
+#include "Texture2D.hpp"
+
+namespace Engine {
+    namespace Graphics {
+
+
+        class Shader {
+
+        public:
+
+            enum ProgramPartType {
+                ProgramPartTypeVertex = GL_VERTEX_SHADER,
+                ProgramPartTypeFragment = GL_FRAGMENT_SHADER,
+            };
+
+            struct ProgramPart {
+                std::string code;
+                ProgramPartType type;
+            };
+
+
+            Shader(std::vector<ProgramPart> parts, std::vector<std::string> defines);
+
+            Shader(std::vector<ProgramPart> parts);
+
+            ~Shader();
+
+            void Bind();
+
+            void Unbind();
+
+            void SetValue(const std::string &name, bool value);
+
+            void SetValue(const std::string &name, int32_t value);
+
+            void SetValue(const std::string &name, float_t value);
+
+            void SetValue(const std::string &name, glm::vec2 value);
+
+            void SetValue(const std::string &name, glm::vec3 value);
+
+            void SetValue(const std::string &name, glm::vec4 value);
+
+            void SetValue(const std::string &name, glm::mat3 value);
+
+            void SetValue(const std::string &name, glm::mat4 value);
+
+            /**
+             * Binds the texture to the texture unit and the uniform also.
+             * @param name The name of the uniform variable
+             * @param texture The texture that should be bound.
+             * @param unit The texture unit the texture should be bound to.
+             * @note The texture unit should be between GL_TEXTURE0-GL_TEXTURE_MAX
+             */
+            void SetValue(const std::string &name, Texture2D *texture, uint32_t unit);
+
+        private:
+
+            uint32_t ID;
+
+            static uint32_t currentlyBound;
+
+            std::vector<ProgramPart> parts;
+            std::vector<std::string> defines;
+            std::unordered_map<std::string, GLint> uniformLoactions;
+            std::unordered_set<std::string> foundNames;
+
+            GLint GetUniformLoaction(const std::string &name);
+
+            void Generate();
+
+            void Delete();
+
+            uint32_t CompileShader(ProgramPart &part);
+
+            void InsertDefinesIntoCode();
+
+        };
+
+    }
+}
+
+
+#endif //FLUIDSOLVER_SHADER_HPP
