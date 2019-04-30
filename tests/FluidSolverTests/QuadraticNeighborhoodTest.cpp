4b998357ef2b8fdbe0dc46751de52494a2912373
gruco0002 <dev.gruco0002@icloud.com>
2019-04-30 13:06:20 +0200

    shift and rotation tests

--- tests/FluidSolverTests/QuadraticNeighborhoodTest.cpp
+++ tests/FluidSolverTests/QuadraticNeighborhoodTest.cpp
@@ -4,6 +4,8 @@
 
 #include <core/SimpleParticleCollection.hpp>
 #include <core/QuadraticNeighborhoodSearch.hpp>
+#include <libraries/glm/gtx/matrix_transform_2d.hpp>
+#include <core/interface/Constants.hpp>
 #include "gtest/gtest.h"
 
 
@@ -33,6 +35,12 @@ std::vector<glm::vec2> GetSampleGrid(float start, float step, float stop) {
     return pos;
 }
 
+/**
+ * Checks if a value exists inside a vector
+ * @param val The vector
+ * @param search The value to search
+ * @return True iff search exists in val
+ */
 bool Exists(std::vector<uint32_t> &val, uint32_t search) {
     return std::find(val.begin(), val.end(), search) != val.end();
 }
@@ -149,6 +157,324 @@ TEST(QuadraticNeighborhoodTest, NeighborhoodCorrect2) {
     for (auto pos : positions) {
         FluidSolver::SimpleParticleCollection::FluidParticle p;
         p.Position = pos;
+        particles.push_back(p);
+    }
+    FluidSolver::INeighborhoodSearch *neighborhoodSearch = GenerateNeighborhoodSearch();
+    FluidSolver::SimpleParticleCollection collection(particles);
+    for (uint32_t i = 0; i < collection.GetSize(); i++) {
+        neighborhoodSearch->FindNeighbors(i, &collection, radius);
+    }
+
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 0)); // top left
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 1));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 5));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 6));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 2));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 11));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 7));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(0), 24), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(0), 12), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(0), 3), false);
+
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 20)); // bottom left
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 16));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 17));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 22));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(20), 7), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(20), 23), false);
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 12)); // center
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 7));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 6));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 13));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 10));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 3));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 0), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 20), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 24), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 4), false);
+
+
+}
+
+
+TEST(QuadraticNeighborhoodTest, ShiftedNeighborhoodCount1) {
+
+    float radius = 1.5f;
+    auto positions = GetSampleGrid(-2.0f, 1.0f, 2.0f);
+
+
+    std::vector<FluidSolver::SimpleParticleCollection::FluidParticle> particles;
+    for (auto pos : positions) {
+        FluidSolver::SimpleParticleCollection::FluidParticle p;
+        p.Position = pos + glm::vec2(10.333f);
+        particles.push_back(p);
+    }
+    FluidSolver::INeighborhoodSearch *neighborhoodSearch = GenerateNeighborhoodSearch();
+    FluidSolver::SimpleParticleCollection collection(particles);
+    for (uint32_t i = 0; i < collection.GetSize(); i++) {
+        neighborhoodSearch->FindNeighbors(i, &collection, radius);
+    }
+
+
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(0).size(), 4); // top left
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(20).size(), 4); // bottom left
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(12).size(), 9); // center
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(18).size(), 9); // center bottom right
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(10).size(), 6); // border left center
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(14).size(), 6); // border right center
+
+}
+
+TEST(QuadraticNeighborhoodTest, ShiftedNeighborhoodCount2) {
+
+    float radius = 2.5f;
+    auto positions = GetSampleGrid(-2.0f, 1.0f, 2.0f);
+
+
+    std::vector<FluidSolver::SimpleParticleCollection::FluidParticle> particles;
+    for (auto pos : positions) {
+        FluidSolver::SimpleParticleCollection::FluidParticle p;
+        p.Position = pos - glm::vec2(50.856f);;
+        particles.push_back(p);
+    }
+    FluidSolver::INeighborhoodSearch *neighborhoodSearch = GenerateNeighborhoodSearch();
+    FluidSolver::SimpleParticleCollection collection(particles);
+    for (uint32_t i = 0; i < collection.GetSize(); i++) {
+        neighborhoodSearch->FindNeighbors(i, &collection, radius);
+    }
+
+
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(0).size(), 8); // top left
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(20).size(), 8); // bottom left
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(12).size(), 21); // center
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(18).size(), 15); // center bottom right
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(10).size(), 13); // border left center
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(14).size(), 13); // border right center
+
+
+}
+
+TEST(QuadraticNeighborhoodTest, ShiftedNeighborhoodCorrect1) {
+
+    float radius = 1.5f;
+    auto positions = GetSampleGrid(-2.0f, 1.0f, 2.0f);
+
+
+    std::vector<FluidSolver::SimpleParticleCollection::FluidParticle> particles;
+    for (auto pos : positions) {
+        FluidSolver::SimpleParticleCollection::FluidParticle p;
+        p.Position = pos + glm::vec2(10.3333f, -46.8453f);
+        particles.push_back(p);
+    }
+    FluidSolver::INeighborhoodSearch *neighborhoodSearch = GenerateNeighborhoodSearch();
+    FluidSolver::SimpleParticleCollection collection(particles);
+    for (uint32_t i = 0; i < collection.GetSize(); i++) {
+        neighborhoodSearch->FindNeighbors(i, &collection, radius);
+    }
+
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 0)); // top left
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 1));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 5));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 6));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(0), 2), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(0), 24), false);
+
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 20)); // bottom left
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 16));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(20), 7), false);
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 12)); // center
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 7));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 6));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 13));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 2), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 20), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 22), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 0), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 19), false);
+
+
+}
+
+TEST(QuadraticNeighborhoodTest, ShiftedNeighborhoodCorrect2) {
+
+    float radius = 2.5f;
+    auto positions = GetSampleGrid(-2.0f, 1.0f, 2.0f);
+
+
+    std::vector<FluidSolver::SimpleParticleCollection::FluidParticle> particles;
+    for (auto pos : positions) {
+        FluidSolver::SimpleParticleCollection::FluidParticle p;
+        p.Position = pos + glm::vec2(10000.0f, -1000000.0f);
+        particles.push_back(p);
+    }
+    FluidSolver::INeighborhoodSearch *neighborhoodSearch = GenerateNeighborhoodSearch();
+    FluidSolver::SimpleParticleCollection collection(particles);
+    for (uint32_t i = 0; i < collection.GetSize(); i++) {
+        neighborhoodSearch->FindNeighbors(i, &collection, radius);
+    }
+
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 0)); // top left
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 1));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 5));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 6));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 2));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 11));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 7));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(0), 24), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(0), 12), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(0), 3), false);
+
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 20)); // bottom left
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 16));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 17));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 22));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(20), 7), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(20), 23), false);
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 12)); // center
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 7));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 6));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 13));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 10));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 3));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 0), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 20), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 24), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 4), false);
+
+
+}
+
+TEST(QuadraticNeighborhoodTest, RotatedNeighborhoodCount1) {
+
+    float radius = 1.5f;
+    auto positions = GetSampleGrid(-2.0f, 1.0f, 2.0f);
+
+    float rotation = 138.0f; // degrees
+    std::vector<FluidSolver::SimpleParticleCollection::FluidParticle> particles;
+    glm::mat3 rot = glm::rotate(glm::mat3(1.0f), FS_PI / 180.0f * rotation);
+    for (auto pos : positions) {
+        FluidSolver::SimpleParticleCollection::FluidParticle p;
+        glm::vec3 rotated = glm::vec3(pos, 1.0f) * rot;
+        p.Position = glm::vec2(rotated.x, rotated.y);
+        particles.push_back(p);
+    }
+    FluidSolver::INeighborhoodSearch *neighborhoodSearch = GenerateNeighborhoodSearch();
+    FluidSolver::SimpleParticleCollection collection(particles);
+    for (uint32_t i = 0; i < collection.GetSize(); i++) {
+        neighborhoodSearch->FindNeighbors(i, &collection, radius);
+    }
+
+
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(0).size(), 4); // top left
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(20).size(), 4); // bottom left
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(12).size(), 9); // center
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(18).size(), 9); // center bottom right
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(10).size(), 6); // border left center
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(14).size(), 6); // border right center
+
+}
+
+TEST(QuadraticNeighborhoodTest, RotatedNeighborhoodCount2) {
+
+    float radius = 2.5f;
+    auto positions = GetSampleGrid(-2.0f, 1.0f, 2.0f);
+
+
+    float rotation = 67.0f; // degrees
+    std::vector<FluidSolver::SimpleParticleCollection::FluidParticle> particles;
+    glm::mat3 rot = glm::rotate(glm::mat3(1.0f), FS_PI / 180.0f * rotation);
+    for (auto pos : positions) {
+        FluidSolver::SimpleParticleCollection::FluidParticle p;
+        glm::vec3 rotated = glm::vec3(pos, 1.0f) * rot;
+        p.Position = glm::vec2(rotated.x, rotated.y);
+        particles.push_back(p);
+    }
+    FluidSolver::INeighborhoodSearch *neighborhoodSearch = GenerateNeighborhoodSearch();
+    FluidSolver::SimpleParticleCollection collection(particles);
+    for (uint32_t i = 0; i < collection.GetSize(); i++) {
+        neighborhoodSearch->FindNeighbors(i, &collection, radius);
+    }
+
+
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(0).size(), 8); // top left
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(20).size(), 8); // bottom left
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(12).size(), 21); // center
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(18).size(), 15); // center bottom right
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(10).size(), 13); // border left center
+    ASSERT_EQ(neighborhoodSearch->GetParticleNeighbors(14).size(), 13); // border right center
+
+
+}
+
+TEST(QuadraticNeighborhoodTest, RotatedNeighborhoodCorrect1) {
+
+    float radius = 1.5f;
+    auto positions = GetSampleGrid(-2.0f, 1.0f, 2.0f);
+
+
+    float rotation = 35.0f; // degrees
+    std::vector<FluidSolver::SimpleParticleCollection::FluidParticle> particles;
+    glm::mat3 rot = glm::rotate(glm::mat3(1.0f), FS_PI / 180.0f * rotation);
+    for (auto pos : positions) {
+        FluidSolver::SimpleParticleCollection::FluidParticle p;
+        glm::vec3 rotated = glm::vec3(pos, 1.0f) * rot;
+        p.Position = glm::vec2(rotated.x, rotated.y);
+        particles.push_back(p);
+    }
+    FluidSolver::INeighborhoodSearch *neighborhoodSearch = GenerateNeighborhoodSearch();
+    FluidSolver::SimpleParticleCollection collection(particles);
+    for (uint32_t i = 0; i < collection.GetSize(); i++) {
+        neighborhoodSearch->FindNeighbors(i, &collection, radius);
+    }
+
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 0)); // top left
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 1));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 5));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(0), 6));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(0), 2), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(0), 24), false);
+
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 20)); // bottom left
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(20), 16));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(20), 7), false);
+
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 12)); // center
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 7));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 6));
+    ASSERT_TRUE(Exists(neighborhoodSearch->GetParticleNeighbors(12), 13));
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 2), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 20), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 22), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 0), false);
+    ASSERT_EQ(Exists(neighborhoodSearch->GetParticleNeighbors(12), 19), false);
+
+
+}
+
+TEST(QuadraticNeighborhoodTest, RotatedNeighborhoodCorrect2) {
+
+    float radius = 2.5f;
+    auto positions = GetSampleGrid(-2.0f, 1.0f, 2.0f);
+
+
+    float rotation = 246.56f; // degrees
+    std::vector<FluidSolver::SimpleParticleCollection::FluidParticle> particles;
+    glm::mat3 rot = glm::rotate(glm::mat3(1.0f), FS_PI / 180.0f * rotation);
+    for (auto pos : positions) {
+        FluidSolver::SimpleParticleCollection::FluidParticle p;
+        glm::vec3 rotated = glm::vec3(pos, 1.0f) * rot;
+        p.Position = glm::vec2(rotated.x, rotated.y);
         particles.push_back(p);
     }
     FluidSolver::INeighborhoodSearch *neighborhoodSearch = GenerateNeighborhoodSearch();
