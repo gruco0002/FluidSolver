0e52b493320c7090fa8379b037fc02bc0249b4d3
gruco0002 <dev.gruco0002@icloud.com>
2019-05-02 17:14:59 +0200

    added some comments

--- src/core/interface/IKernel.hpp
+++ src/core/interface/IKernel.hpp
@@ -15,12 +15,42 @@ namespace FluidSolver {
 
     public:
 
+        /**
+         * This should return a kernel value evaluated at the given position relative to the origin.
+         * @param position Position to evaluate the kernel.
+         * @param kernelSupport The kernel support area.
+         * @return Kernel value.
+         * @note The kernelSupport is the distance from the origin to the location, after which the kernel function evaluates to 0.
+         */
         virtual float GetKernelValue(glm::vec2 position, float kernelSupport) = 0;
 
+        /**
+         * Helper function calling GetKernelValue with the position := neighborPosition - position
+         * @see FluidSolver::IKernel::GetKernelValue
+         * @param neighborPosition
+         * @param position
+         * @param kernelSupport
+         * @return
+         */
         virtual float GetKernelValue(glm::vec2 neighborPosition, glm::vec2 position, float kernelSupport);
 
+        /**
+         * This should return a non reversed kernel derivative value evaluated at the given position relative to the origin.
+         * @param position Position to evaluate the kernel.
+         * @param kernelSupport The kernel support area.
+         * @return Returns a non reversed kernel derivative value.
+         * @note The kernelSupport is the distance from the origin to the location, after which the kernel function evaluates to 0.
+         */
         virtual glm::vec2 GetKernelDerivativeValue(glm::vec2 position, float kernelSupport) = 0;
 
+        /**
+         * Helper function calling GetKernelDerivativeValue with the position := neighborPosition - position
+         * @see FluidSolver::IKernel::GetKernelDerivativeValue
+         * @param neighborPosition
+         * @param position
+         * @param kernelSupport 
+         * @return
+         */
         virtual glm::vec2 GetKernelDerivativeValue(glm::vec2 neighborPosition, glm::vec2 position, float kernelSupport);
 
 
