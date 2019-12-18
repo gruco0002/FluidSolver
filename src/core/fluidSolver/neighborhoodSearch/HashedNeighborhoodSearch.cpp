093ccc46a12045ec8377019f6243fb08c7bf97f3
gruco0002 <dev.gruco0002@icloud.com>
2019-12-18 22:46:12 +0100

    improved hashed neighborhood search

--- src/core/fluidSolver/neighborhoodSearch/HashedNeighborhoodSearch.cpp
+++ src/core/fluidSolver/neighborhoodSearch/HashedNeighborhoodSearch.cpp
@@ -8,30 +8,8 @@
 void FluidSolver::HashedNeighborhoodSearch::FindNeighbors(FluidSolver::IParticleCollection *particleCollection,
                                                           float radius) {
 
-    uint32_t particleCount = particleCollection->GetSize();
-    // set every particle to the correct grid cell
-    for (uint32_t i = 0; i < particleCount; i++) {
-        // also neighbors have to be updated in the grid
-
-        auto key = GetGridCellByParticleID(i, particleCollection);
-        if (currentStatus[i] == key)
-            continue;
-
-        // otherwise the gridcell of the particle changed
-
-        // remove the particle from the old gridcell
-        gridToParticles[currentStatus[i]].remove(i);
-
-        // update current status
-        currentStatus[i] = key;
-
-        // add to new grid cell
-        CreateGridEntryIfNecessary(key);
-        gridToParticles[key].push_back(i);
-
-    }
-
-
+    // First update the grid
+    UpdateGrid(particleCollection);
 
     // call neighborhood search for each particle that is not a boundary particle
     INeighborhoodSearch::FindNeighbors(particleCollection, radius);
@@ -103,11 +81,7 @@ FluidSolver::HashedNeighborhoodSearch::GetGridCellByParticleID(uint32_t particle
                                                                FluidSolver::IParticleCollection *particleCollection) {
 
     glm::vec2 pos = particleCollection->GetPosition(particleIndex);
-    float tmpx = pos.x / gridCellSize;
-    float tmpy = pos.y / gridCellSize;
-    int32_t x = static_cast<int32_t>(tmpx);
-    int32_t y = static_cast<int32_t>(tmpy);
-    return {x, y};
+    return GetGridCellByPosition(pos);
 }
 
 void
@@ -119,3 +93,81 @@ FluidSolver::HashedNeighborhoodSearch::CreateGridEntryIfNecessary(FluidSolver::H
 }
 
 FluidSolver::HashedNeighborhoodSearch::HashedNeighborhoodSearch(float gridCellSize) : gridCellSize(gridCellSize) {}
+
+std::vector<uint32_t>
+FluidSolver::HashedNeighborhoodSearch::GetParticleNeighborsByPosition(glm::vec2 position, float radius,
+                                                                      IParticleCollection *particleCollection) {
+
+    // get grid cell
+    GridKey gridCell = GetGridCellByPosition(position);
+
+    // determine the grid cells needed to check
+    std::vector<GridKey> toCheck;
+    int32_t radiusInCellsFromTheCenter = ceil(radius / gridCellSize);
+
+    for (int32_t x = gridCell.first - radiusInCellsFromTheCenter;
+         x <= gridCell.first + radiusInCellsFromTheCenter; x++) {
+        for (int32_t y = gridCell.second - radiusInCellsFromTheCenter;
+             y <= gridCell.second + radiusInCellsFromTheCenter; y++) {
+            toCheck.push_back({x, y});
+        }
+    }
+
+    // create result vector
+    std::vector<uint32_t> res;
+
+    // iterate over the grid cells and their particles to check for neighbors
+    for (GridKey &key : toCheck) {
+        if (gridToParticles.find(key) == gridToParticles.end())
+            continue;
+
+        for (uint32_t particle : gridToParticles[key]) {
+            glm::vec2 distVec = position - particleCollection->GetPosition(particle);
+            if (glm::length(distVec) <= radius) {
+                // this is a neighbor, add it
+                res.push_back(particle);
+            }
+        }
+    }
+
+    return res;
+}
+
+FluidSolver::HashedNeighborhoodSearch::GridKey
+FluidSolver::HashedNeighborhoodSearch::GetGridCellByPosition(glm::vec2 &pos) {
+    float tmpx = pos.x / gridCellSize;
+    float tmpy = pos.y / gridCellSize;
+    int32_t x = static_cast<int32_t>(tmpx);
+    int32_t y = static_cast<int32_t>(tmpy);
+    return {x, y};
+}
+
+void FluidSolver::HashedNeighborhoodSearch::UpdateGrid(FluidSolver::IParticleCollection *particleCollection) {
+    uint32_t particleCount = particleCollection->GetSize();
+    // set every particle to the correct grid cell
+    for (uint32_t i = 0; i < particleCount; i++) {
+        // also neighbors have to be updated in the grid
+
+        auto key = GetGridCellByParticleID(i, particleCollection);
+        if (currentStatus[i] == key)
+            continue;
+
+        // otherwise the gridcell of the particle changed
+
+        // remove the particle from the old gridcell
+        gridToParticles[currentStatus[i]].remove(i);
+
+        // update current status
+        currentStatus[i] = key;
+
+        // add to new grid cell
+        CreateGridEntryIfNecessary(key);
+        gridToParticles[key].push_back(i);
+
+    }
+}
+
+void FluidSolver::HashedNeighborhoodSearch::SetupForPositionNeighborSearch(
+        FluidSolver::IParticleCollection *particleCollection) {
+    UpdateGrid(particleCollection);
+}
