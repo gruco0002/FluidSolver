16920cdec9a84d9ca00b8dcb34e3aa644e2b89a7
gruco0002 <dev.gruco0002@icloud.com>
2020-03-19 16:03:38 +0100

    compact particle collection

--- src/core/fluidSolver/particleCollection/CompactParticleCollection.cpp
+++ src/core/fluidSolver/particleCollection/CompactParticleCollection.cpp
@@ -1,2 +1,146 @@
 
 #include "CompactParticleCollection.hpp"
+
+size_t FluidSolver::CompactParticleCollection::GetParticleID(size_t index) {
+    return particleIndexToParticleIDMap[index];
+}
+
+size_t FluidSolver::CompactParticleCollection::GetIndex(size_t particleID) {
+    return particleIDToParticleIndexMap[particleID];
+}
+
+size_t FluidSolver::CompactParticleCollection::AddParticle(const FluidSolver::FluidParticle &fluidParticle) {
+
+    auto index = AddEmptyParticle();
+
+    particleData[index] = fluidParticle;
+
+    return index;
+}
+
+size_t FluidSolver::CompactParticleCollection::AddEmptyParticle() {
+    // get the new particle index
+    size_t index = particleData.size();
+
+    // create empty particle data
+    particleData.push_back(FluidParticle());
+
+    // set the index mapping data
+    particleIDToParticleIndexMap.push_back(index);
+    particleIndexToParticleIDMap.push_back(index);
+    sortKeys.push_back(0);
+
+    return index;
+}
+
+float FluidSolver::CompactParticleCollection::GetMass(uint32_t index) {
+    return particleData[index].Mass;
+}
+
+void FluidSolver::CompactParticleCollection::SetMass(uint32_t index, float value) {
+    particleData[index].Mass = value;
+}
+
+float FluidSolver::CompactParticleCollection::GetPressure(uint32_t index) {
+    return particleData[index].Pressure;
+}
+
+void FluidSolver::CompactParticleCollection::SetPressure(uint32_t index, float value) {
+    particleData[index].Pressure = value;
+}
+
+glm::vec2 FluidSolver::CompactParticleCollection::GetPosition(uint32_t index) {
+    return particleData[index].Position;
+}
+
+void FluidSolver::CompactParticleCollection::SetPosition(uint32_t index, glm::vec2 value) {
+    particleData[index].Position = value;
+}
+
+glm::vec2 FluidSolver::CompactParticleCollection::GetVelocity(uint32_t index) {
+    return particleData[index].Velocity;
+}
+
+void FluidSolver::CompactParticleCollection::SetVelocity(uint32_t index, glm::vec2 value) {
+    particleData[index].Velocity = value;
+}
+
+glm::vec2 FluidSolver::CompactParticleCollection::GetAcceleration(uint32_t index) {
+    return particleData[index].Acceleration;
+}
+
+void FluidSolver::CompactParticleCollection::SetAcceleration(uint32_t index, glm::vec2 value) {
+    particleData[index].Acceleration = value;
+}
+
+uint32_t FluidSolver::CompactParticleCollection::GetSize() {
+    return particleData.size();
+}
+
+float FluidSolver::CompactParticleCollection::GetDensity(uint32_t index) {
+    return particleData[index].Density;
+}
+
+void FluidSolver::CompactParticleCollection::SetDensity(uint32_t index, float value) {
+    particleData[index].Density = value;
+}
+
+FluidSolver::ParticleType FluidSolver::CompactParticleCollection::GetParticleType(uint32_t index) {
+    return (FluidSolver::ParticleType) particleData[index].Type;
+}
+
+void FluidSolver::CompactParticleCollection::SetParticleType(uint32_t index, FluidSolver::ParticleType value) {
+    particleData[index].Type = value;
+}
+
+glm::vec2 FluidSolver::CompactParticleCollection::GetNonPressureAcceleration(uint32_t index) {
+    return particleData[index].NonPressureAcceleration;
+}
+
+void FluidSolver::CompactParticleCollection::SetNonPressureAcceleration(uint32_t index, glm::vec2 value) {
+    particleData[index].NonPressureAcceleration = value;
+}
+
+glm::vec2 FluidSolver::CompactParticleCollection::GetPredictedVelocity(uint32_t index) {
+    return particleData[index].PredictedVelocity;
+}
+
+void FluidSolver::CompactParticleCollection::SetPredictedVelocity(uint32_t index, glm::vec2 value) {
+    particleData[index].PredictedVelocity = value;
+}
+
+float FluidSolver::CompactParticleCollection::GetSourceTerm(uint32_t index) {
+    return particleData[index].SourceTerm;
+}
+
+void FluidSolver::CompactParticleCollection::SetSourceTerm(uint32_t index, float value) {
+    particleData[index].SourceTerm = value;
+}
+
+float FluidSolver::CompactParticleCollection::GetDiagonalElement(uint32_t index) {
+    return particleData[index].DiagonalElement;
+}
+
+void FluidSolver::CompactParticleCollection::SetDiagonalElement(uint32_t index, float value) {
+    particleData[index].DiagonalElement = value;
+}
+
+uint64_t FluidSolver::CompactParticleCollection::GetSortKey(size_t index) {
+    return sortKeys[index];
+}
+
+void FluidSolver::CompactParticleCollection::PrecalculateSortKeys(
+        const FluidSolver::IParticleCollection::sortKeyFunction_t &sortKeyFunction) {
+    // TODO: parallelization
+    for (size_t i = 0; i < GetSize(); i++) {
+        sortKeys[i] = sortKeyFunction(i);
+    }
+}
+
+void FluidSolver::CompactParticleCollection::SwapElements(size_t i, size_t j) {
+    std::swap(sortKeys[i], sortKeys[j]);
+    std::swap(particleIndexToParticleIDMap[i], particleIndexToParticleIDMap[j]);
+    std::swap(particleIDToParticleIndexMap[i], particleIDToParticleIndexMap[j]);
+
+    std::swap(particleData[i], particleData[j]);
+}
--- src/core/fluidSolver/particleCollection/CompactParticleCollection.hpp
+++ src/core/fluidSolver/particleCollection/CompactParticleCollection.hpp
@@ -1,9 +1,87 @@
-
 #ifndef FLUIDSOLVER_COMPACTPARTICLECOLLECTION_HPP
 #define FLUIDSOLVER_COMPACTPARTICLECOLLECTION_HPP
 
-namespace {
-    class CompactParticleCollection {
+#include "IParticleCollection.hpp"
+#include <vector>
+
+namespace FluidSolver {
+
+    /**
+     * The compact particle collection stores the particle data as compact vector of a particle structure containing all
+     * the data of one particle.
+     * For more information see FluidSolver::IParticleCollection
+     */
+    class CompactParticleCollection : public IParticleCollection {
+
+    private:
+
+        std::vector<uint64_t> sortKeys;
+        std::vector<size_t> particleIDToParticleIndexMap;
+        std::vector<size_t> particleIndexToParticleIDMap;
+
+        std::vector<FluidParticle> particleData;
+
+    public:
+        size_t GetParticleID(size_t index) override;
+
+        size_t GetIndex(size_t particleID) override;
+
+        size_t AddParticle(const FluidParticle &fluidParticle) override;
+
+        size_t AddEmptyParticle() override;
+
+        float GetMass(uint32_t index) override;
+
+        void SetMass(uint32_t index, float value) override;
+
+        float GetPressure(uint32_t index) override;
+
+        void SetPressure(uint32_t index, float value) override;
+
+        glm::vec2 GetPosition(uint32_t index) override;
+
+        void SetPosition(uint32_t index, glm::vec2 value) override;
+
+        glm::vec2 GetVelocity(uint32_t index) override;
+
+        void SetVelocity(uint32_t index, glm::vec2 value) override;
+
+        glm::vec2 GetAcceleration(uint32_t index) override;
+
+        void SetAcceleration(uint32_t index, glm::vec2 value) override;
+
+        uint32_t GetSize() override;
+
+        float GetDensity(uint32_t index) override;
+
+        void SetDensity(uint32_t index, float value) override;
+
+        ParticleType GetParticleType(uint32_t index) override;
+
+        void SetParticleType(uint32_t index, ParticleType value) override;
+
+        glm::vec2 GetNonPressureAcceleration(uint32_t index) override;
+
+        void SetNonPressureAcceleration(uint32_t index, glm::vec2 value) override;
+
+        glm::vec2 GetPredictedVelocity(uint32_t index) override;
+
+        void SetPredictedVelocity(uint32_t index, glm::vec2 value) override;
+
+        float GetSourceTerm(uint32_t index) override;
+
+        void SetSourceTerm(uint32_t index, float value) override;
+
+        float GetDiagonalElement(uint32_t index) override;
+
+        void SetDiagonalElement(uint32_t index, float value) override;
+
+    protected:
+        uint64_t GetSortKey(size_t index) override;
+
+        void PrecalculateSortKeys(const sortKeyFunction_t &sortKeyFunction) override;
+
+        void SwapElements(size_t i, size_t j) override;
 
     };
 }
