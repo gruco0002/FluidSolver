573c61c298224fce07841faa58a06af72c7f7c45
gruco0002 <dev.gruco0002@icloud.com>
2020-04-18 11:46:20 +0200

    added more tests

--- tests/FluidSolverTests/CompactHashingComponentTests/CompactHashingCellStorageTests.cpp
+++ tests/FluidSolverTests/CompactHashingComponentTests/CompactHashingCellStorageTests.cpp
@@ -246,3 +246,50 @@ TEST(CompactHashingCellStorageTest, InsertOverfill) {
 
 
 }
+
+TEST(CompactHashingCellStorageTest, RemoveMultipleOccurencesOfTheSameValue) {
+
+    auto storage = FluidSolver::CompactHashingNeighborhoodSearch::CellStorage(12);
+    auto storageSection = storage.GetEmptyStorageSection();
+    auto gridKey = FluidSolver::CompactHashingNeighborhoodSearch::GridCell(0, 0);
+
+
+    storage.AddParticleToStorageSection(storageSection, 4, gridKey);
+    for (FluidSolver::particleIndex_t index = 0; index < 9; index++) {
+        storage.AddParticleToStorageSection(storageSection, index, gridKey);
+    }
+    storage.AddParticleToStorageSection(storageSection, 4, gridKey);
+
+    // checking for correct insertion
+    EXPECT_EQ(11, storage.GetStorageSectionElementCount(storageSection));
+    EXPECT_EQ(gridKey, storage.GetStorageSectionGridCell(storageSection));
+
+    auto begin = storage.GetStorageSectionDataBegin(storageSection);
+    auto end = storage.GetStorageSectionDataEnd(storageSection);
+    std::vector<FluidSolver::particleIndex_t> data;
+    for (auto current = begin; current != end; current++) {
+        data.push_back((*current).particleIndex.value);
+    }
+
+    EXPECT_EQ(11, data.size());
+    EXPECT_THAT(data, UnorderedElementsAre(0, 1, 2, 3, 4, 5, 6, 7, 8, 4, 4));
+
+    // removing all occurences of 4
+    storage.RemoveParticleFromStorageSection(storageSection, 4);
+
+    // checking new state
+    EXPECT_EQ(8, storage.GetStorageSectionElementCount(storageSection));
+    EXPECT_EQ(gridKey, storage.GetStorageSectionGridCell(storageSection));
+
+    auto begin2 = storage.GetStorageSectionDataBegin(storageSection);
+    auto end2 = storage.GetStorageSectionDataEnd(storageSection);
+    std::vector<FluidSolver::particleIndex_t> data2;
+    for (auto current = begin2; current != end2; current++) {
+        data2.push_back((*current).particleIndex.value);
+    }
+
+    EXPECT_EQ(8, data2.size());
+    EXPECT_THAT(data2, UnorderedElementsAre(0, 1, 2, 3, 5, 6, 7,8));
+
+
+}
