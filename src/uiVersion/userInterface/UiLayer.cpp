77d7dbebac90060650645dbe9430f4607b37932b
gruco0002 <dev.gruco0002@icloud.com>
2020-10-24 22:55:31 +0200

    Allowing to change the types

--- src/uiVersion/userInterface/UiLayer.cpp
+++ src/uiVersion/userInterface/UiLayer.cpp
@@ -49,6 +49,77 @@ void FluidUi::UiLayer::render() {
         scenarios.render();
     });
 
+    BeginSubsection("Solver Setup", [=]() {
+
+        bool can_change = !window->asynchronous_simulation || (!window->running && window->is_done_working());
+
+        if (ImGui::BeginCombo("Solver", window->current_type->name_solver.c_str())) {
+            for (auto &name : window->solver_types.names_solver) {
+                if (ImGui::Selectable(name.c_str(), window->current_type->name_solver == name)) {
+                    FluidSolverTypes::FluidSolverTypeQuery q;
+                    q.name_solver = name;
+                    q.name_neighborhood_search = window->current_type->name_neighborhood_search;
+                    q.name_kernel = window->current_type->name_kernel;
+                    auto t = window->solver_types.query_type(q);
+                    if (t && can_change) {
+                        window->current_type = t;
+
+                        // set the new type
+                        delete window->simulation.parameters.fluid_solver;
+                        window->simulation.parameters.fluid_solver = t->create_type();
+                    }
+                }
+            }
+            ImGui::EndCombo();
+        }
+
+        if (ImGui::BeginCombo("N. Search", window->current_type->name_neighborhood_search.c_str())) {
+            for (auto &name : window->solver_types.names_neighborhood_search) {
+                if (ImGui::Selectable(name.c_str(), window->current_type->name_neighborhood_search == name)) {
+                    FluidSolverTypes::FluidSolverTypeQuery q;
+                    q.name_solver = window->current_type->name_solver;
+                    q.name_neighborhood_search = name;
+                    q.name_kernel = window->current_type->name_kernel;
+                    auto t = window->solver_types.query_type(q);
+                    if (t && can_change) {
+                        window->current_type = t;
+
+                        // set the new type
+                        delete window->simulation.parameters.fluid_solver;
+                        window->simulation.parameters.fluid_solver = t->create_type();
+                    }
+                }
+            }
+            ImGui::EndCombo();
+        }
+
+        if (ImGui::BeginCombo("Kernel", window->current_type->name_kernel.c_str())) {
+            for (auto &name : window->solver_types.names_kernel) {
+                if (ImGui::Selectable(name.c_str(), window->current_type->name_kernel == name)) {
+                    FluidSolverTypes::FluidSolverTypeQuery q;
+                    q.name_solver = window->current_type->name_solver;
+                    q.name_neighborhood_search = window->current_type->name_neighborhood_search;
+                    q.name_kernel = name;
+                    auto t = window->solver_types.query_type(q);
+                    if (t && can_change) {
+                        window->current_type = t;
+
+                        // set the new type
+                        delete window->simulation.parameters.fluid_solver;
+                        window->simulation.parameters.fluid_solver = t->create_type();
+                    }
+                }
+            }
+            ImGui::EndCombo();
+        }
+
+        if (!can_change) {
+            ImGui::TextColored(ImColor(0.8f, 0.1f, 0.1f), "Config change not possible");
+            ImGui::TextWrapped(
+                    "Since you are running the simulation in asynchronous mode, you cannot change the solver setup if the simulation is running or has not stopped running!");
+        }
+
+    });
 
     ImGui::End();
 
