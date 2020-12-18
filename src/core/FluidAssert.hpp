82a482add0c477106091249d6a2b45084cbd0bf0
gruco0002 <dev.gruco0002@icloud.com>
2020-12-18 17:41:30 +0100

    improved FLUID_ASSERT to trigger MSVC breakpoint

--- src/core/FluidAssert.hpp
+++ src/core/FluidAssert.hpp
@@ -7,13 +7,20 @@
     #define USE_FLUID_ASSERT
 #endif
 
+#ifdef _MSC_VER
+    #define FLUID_BREAK __debugbreak()
+#else
+    #define FLUID_BREAK abort()
+#endif
+
+
 #ifdef USE_FLUID_ASSERT
     #define FLUID_ASSERT(condition)                                     \
         {                                                               \
             if (!(condition)) {                                         \
                 std::cerr << "Assertion failed: " << #condition << std::endl; \
                 std::cerr << "This happened in " << __FILE__ << ":" << __LINE__ << " in " << __FUNCTION__ << std::endl; \
-                abort();                                          \
+                FLUID_BREAK;                                          \
             }                                                            \
         }
 #else
