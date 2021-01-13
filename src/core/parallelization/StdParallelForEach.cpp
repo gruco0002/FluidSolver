f9729f4bbc5222fc626854777b70c072f3e299f5
gruco0002 <dev.gruco0002@icloud.com>
2021-01-13 22:18:22 +0100

    added chunked parallel for implementation

--- src/core/parallelization/StdParallelForEach.cpp
+++ src/core/parallelization/StdParallelForEach.cpp
@@ -57,20 +57,30 @@ public:
 void FluidSolver::StdParallelForEach::loop_for(size_t from, size_t to, const std::function<void(size_t i)>& fn)
 {
 #ifndef __clang__
-	std::for_each(std::execution::par, SizeTIterator(from), SizeTIterator(to), fn);
+
+	constexpr size_t chunk_size = 16;
+	size_t chunked_to = from + (to - from) / chunk_size;
+	if ((to - from) % chunk_size != 0) chunked_to++;
+
+	std::for_each(std::execution::par, SizeTIterator(from), SizeTIterator(chunked_to), [chunk_size, &fn, to](size_t i) {
+		size_t base = i * chunk_size;
+		for (uint8_t j = 0; j < chunk_size && base + j < to; j++) {
+			fn(base + j);
+		}
+		});
 #else
-    #warning Apple Clang Compiler does not support StdParallelForEach. Currently using non parallel implementation instead.
-    for(size_t i = from; i < to; i++) fn(i);
+	#warning Apple Clang Compiler does not support StdParallelForEach.Currently using non parallel implementation instead.
+		for (size_t i = from; i < to; i++) fn(i);
 #endif
-}
+	}
 
 void FluidSolver::StdParallelForEach::loop_for(size_t from, size_t to, size_t step, const std::function<void(size_t i)>& fn)
 {
 #ifndef __clang__
 	size_t steps = ((to - 1) - from) / step + 1;
-    std::for_each(std::execution::par, SizeTIterator(0), SizeTIterator(steps), [&fn, from, step](size_t i) {fn(from + i * step); });
+	std::for_each(std::execution::par, SizeTIterator(0), SizeTIterator(steps), [&fn, from, step](size_t i) {fn(from + i * step); });
 #else
-    #warning Apple Clang Compiler does not support StdParallelForEach. Currently using non parallel implementation instead.
-    for(size_t i = from; i < to; i += step) fn(i);
+	#warning Apple Clang Compiler does not support StdParallelForEach.Currently using non parallel implementation instead.
+		for (size_t i = from; i < to; i += step) fn(i);
 #endif
 }
