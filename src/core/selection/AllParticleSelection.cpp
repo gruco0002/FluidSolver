596a1e3f96526baa299e3f75b9eea70f629425cb
gruco0002 <dev.gruco0002@icloud.com>
2021-05-06 14:03:14 +0200

    Cleanup old files

--- src/core/selection/AllParticleSelection.cpp
+++
@@ -1,6 +0,0 @@
-#include "AllParticleSelection.hpp"
-
-bool FluidSolver::AllParticleSelection::IsParticleSelected(uint32_t particleIndex,
-                                                           FluidSolver::IParticleCollection *particleCollection) {
-    return true;
-}
--- src/core/selection/NormalParticleSelection.cpp
+++
@@ -1,7 +0,0 @@
-#include "NormalParticleSelection.hpp"
-
-bool FluidSolver::NormalParticleSelection::IsParticleSelected(uint32_t particleIndex,
-                                                              FluidSolver::IParticleCollection *particleCollection) {
-    auto type = particleCollection->GetParticleType(particleIndex);
-    return type == ParticleTypeNormal;
-}
