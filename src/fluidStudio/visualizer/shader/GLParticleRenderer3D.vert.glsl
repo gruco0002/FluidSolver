6cb4bb86b399ceb2fb76720bdd578b6f37567916
gruco0002 <dev.gruco0002@icloud.com>
2021-07-15 14:00:10 +0200

    added better memory location visualization

--- src/fluidStudio/visualizer/shader/GLParticleRenderer3D.vert.glsl
+++ src/fluidStudio/visualizer/shader/GLParticleRenderer3D.vert.glsl
@@ -19,6 +19,13 @@ out VS_OUT {
 } vs_out;
 
 
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 void main()
 {
 
@@ -33,10 +40,8 @@ void main()
 
     if(showParticleMemoryLocation == 1){
         float vertID = float(gl_VertexID);
-        float third = vertID / (numberOfParticles / 3.0);
-        vs_out.color.r = clamp(third, 0.0, 1.0);
-        vs_out.color.g = clamp(third - 1.0f, 0.0, 1.0);
-        vs_out.color.b = clamp(third - 2.0f, 0.0, 1.0);
+        float partOfScale = vertID / numberOfParticles;      
+        vs_out.color.rgb = hsv2rgb(vec3(partOfScale, 1.0, 1.0));
         vs_out.color.a = 1.0;
     }
     
