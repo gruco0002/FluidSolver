5f2b7a0c46c428d04e8c798d88e63858e4fa9938
gruco0002 <dev.gruco0002@icloud.com>
2021-09-12 16:09:05 +0200

    Added Stable Quick Sort Variant using Dutch Flag Algorithm

--- src/libFluid/fluidSolver/ParticleCollectionAlgorithm.hpp
+++ src/libFluid/fluidSolver/ParticleCollectionAlgorithm.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ParticleCollection.hpp"
+#include "parallelization/StdParallelForEach.hpp"
 
 #include <functional>
 #include <future>
@@ -32,7 +33,6 @@ namespace FluidSolver::ParticleCollectionAlgorithm
                 FLUID_ASSERT(collection->is_type_present<SortInfo>());
                 precalculate_keys(collection, key);
             }
-
 
             auto partition = [&](int64_t low, int64_t high) -> int64_t {
                 uint64_t pivot;
@@ -122,6 +122,149 @@ namespace FluidSolver::ParticleCollectionAlgorithm
                         auto lower_part_sorting = std::async(policy, [&]() { quick_sort(low, partitioning_index); });
                         auto upper_part_sorting =
                             std::async(policy, [&]() { quick_sort(partitioning_index + 1, high); });
+
+                        lower_part_sorting.wait();
+                        upper_part_sorting.wait();
+                    }
+                }
+            };
+
+            quick_sort(0, collection->size() - 1);
+        }
+
+        template <bool should_precalculate_keys = true, bool calculate_parallellized = true>
+        void quick_sort_stable(std::shared_ptr<ParticleCollection> collection, const key_function_t& key)
+        {
+            using parallel = StdParallelForEach;
+
+            if constexpr (should_precalculate_keys)
+            {
+                FLUID_ASSERT(collection->is_type_present<SortInfo>());
+                precalculate_keys(collection, key);
+            }
+
+
+            std::vector<uint32_t> original_index;
+            original_index.resize(collection->size());
+            if constexpr (calculate_parallellized)
+            {
+                parallel::loop_for(0, original_index.size(), [&](size_t i) { original_index[i] = i; });
+            }
+            else
+            {
+                for (size_t i = 0; i < original_index.size(); i++)
+                {
+                    original_index[i] = i;
+                }
+            }
+
+            auto three_way_partition = [&](int64_t low, int64_t high) -> std::tuple<int64_t, int64_t> {
+                int64_t pivot_index = low + (high - low) / 2;
+                // getting the pivot value
+                uint64_t pivot_value;
+                if constexpr (should_precalculate_keys)
+                {
+                    pivot_value = collection->get<SortInfo>(pivot_index).key;
+                }
+                else
+                {
+                    pivot_value = key(collection, pivot_index);
+                }
+
+                int64_t i, j;
+                i = low;
+                j = low;
+                int64_t k = high;
+
+                while (j <= k)
+                {
+                    uint64_t value_at_j;
+                    if constexpr (should_precalculate_keys)
+                    {
+                        value_at_j = collection->get<SortInfo>(j).key;
+                    }
+                    else
+                    {
+                        value_at_j = key(collection, j);
+                    }
+
+                    if (value_at_j < pivot_value)
+                    {
+                        collection->swap(i, j);
+                        std::swap(original_index[i], original_index[j]);
+                        i++;
+                        j++;
+                    }
+                    else if (value_at_j > pivot_value)
+                    {
+                        collection->swap(j, k);
+                        std::swap(original_index[j], original_index[k]);
+                        k--;
+                    }
+                    else
+                    {
+                        j++;
+                    }
+                }
+                return {i, j};
+            };
+
+            std::function<void(int64_t, int64_t)> sort_by_original_index = [&](int64_t low, int64_t high) {
+                if (high - low <= 0)
+                {
+                    // nothing to sort
+                    return;
+                }
+
+                // insertion sort
+                int i = low + 1;
+                while (i <= high)
+                {
+                    int j = i;
+                    while (j > low && original_index[j - 1] > original_index[j])
+                    {
+                        collection->swap(j, j - 1);
+                        std::swap(original_index[j], original_index[j - 1]);
+                        j--;
+                    }
+                    i++;
+                }
+            };
+
+
+            std::function<void(int64_t, int64_t)> quick_sort = [&](int64_t low, int64_t high) {
+                if (low >= high)
+                    return;
+
+                auto [p1, p2] = three_way_partition(low, high);
+                if constexpr (!calculate_parallellized)
+                {
+
+                    quick_sort(low, p1 - 1);
+                    sort_by_original_index(p1, p2 - 1);
+                    quick_sort(p2, high);
+                }
+                else
+                {
+                    constexpr size_t MIN_ASYNC_SIZE = 10000;
+
+                    if (p1 - low < MIN_ASYNC_SIZE || high - p2 < MIN_ASYNC_SIZE)
+                    {
+                        // one of the jobs is too small to allow for the overhead that would come with multi threading
+                        // hence do the jobs normally
+                        quick_sort(low, p1 - 1);
+                        sort_by_original_index(p1, p2 - 1);
+                        quick_sort(p2, high);
+                    }
+                    else
+                    {
+                        // both jobs are large enough to be split up on two threads
+
+                        auto policy = std::launch::async;
+                        auto lower_part_sorting = std::async(policy, [&]() { quick_sort(low, p1 - 1); });
+                        auto upper_part_sorting = std::async(policy, [&]() { quick_sort(p2, high); });
+
+                        sort_by_original_index(p1, p2 - 1);
 
                         lower_part_sorting.wait();
                         upper_part_sorting.wait();
