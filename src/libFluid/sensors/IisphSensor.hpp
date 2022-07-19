141fdaba6e4b78d3745f2ff34166c2e508178def
gruco0002 <dev.gruco0002@icloud.com>
2022-07-19 18:27:27 +0200

    Added IISPHSensor

---
+++ src/libFluid/sensors/IisphSensor.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "FluidInclude.hpp"
+#include "sensors/ISensor.hpp"
+#include "sensors/SensorData.hpp"
+
+namespace FluidSolver::Sensors {
+
+    class IISPHSensor : public ISensor {
+      public:
+        struct Info {
+            size_t stat_last_iteration_count = 0;
+            float stat_last_average_predicted_density_error = 0.0f;
+        };
+
+        SensorData<Info> data;
+
+        virtual void calculate_and_store(const Timepoint& timepoint) override;
+        virtual void save_data_to_file(SensorWriter& writer) override;
+
+        virtual void create_compatibility_report(CompatibilityReport& report) override;
+
+      private:
+        bool is_type_compatible() const;
+        size_t get_last_iteration_count() const;
+        float get_last_average_predicted_density_error() const;
+
+      private:
+        static bool try_fetch_data_from_iisph_solver(const std::shared_ptr<IFluidSolverBase>& solver, size_t* last_iteration_count, float* last_average_predicted_density_error);
+    };
+
+} // namespace FluidSolver::Sensors
