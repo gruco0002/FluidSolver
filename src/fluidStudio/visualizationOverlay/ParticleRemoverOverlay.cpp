7553e04f5411fcabced225a5932a6ee33a2d7873
gruco0002 <dev.gruco0002@icloud.com>
2022-07-20 23:21:01 +0200

    Implemented particle remover overlay

--- src/fluidStudio/visualizationOverlay/ParticleRemoverOverlay.cpp
+++ src/fluidStudio/visualizationOverlay/ParticleRemoverOverlay.cpp
@@ -1,5 +1,8 @@
 #include "ParticleRemoverOverlay.hpp"
 
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
+
 namespace FluidUi {
 
     OverlayInstance::Display ParticleRemoverOverlay::get_display() {
@@ -7,13 +10,31 @@ namespace FluidUi {
     }
 
     glm::mat4 ParticleRemoverOverlay::get_matrix() {
-        return glm::mat4();
+        return glm::translate(glm::scale(glm::mat4(1.0f), entity->parameters.volume.distance_from_center), entity->parameters.volume.center);
     }
-    void ParticleRemoverOverlay::set_matrix(const glm::mat4& mat_4) {
 
+    void ParticleRemoverOverlay::set_matrix(const glm::mat4& new_data) {
+        glm::vec3 scale;
+        glm::quat rotation;
+        glm::vec3 translation;
+        glm::vec3 skew;
+        glm::vec4 perspective;
+
+        glm::decompose(new_data, scale, rotation, translation, skew, perspective);
+
+        entity->parameters.volume.center = translation;
+
+        scale.x = abs(scale.x);
+        scale.y = abs(scale.y);
+        scale.z = abs(scale.z);
+
+        entity->parameters.volume.distance_from_center = scale;
     }
 
     OverlayInstance::AllowedTransforms ParticleRemoverOverlay::get_allowed_transforms() {
         return AllowedTransforms::TranslateAndScale;
+    }
+    ParticleRemoverOverlay::ParticleRemoverOverlay(std::shared_ptr<FluidSolver::ParticleRemover3D> entity) {
+        this->entity = entity;
     }
 } // namespace FluidUi
--- src/fluidStudio/visualizationOverlay/ParticleRemoverOverlay.hpp
+++ src/fluidStudio/visualizationOverlay/ParticleRemoverOverlay.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#include "entities/ParticleRemover3D.hpp"
 #include "visualizationOverlay/OverlayInstance.hpp"
 
 namespace FluidUi {
 
     class ParticleRemoverOverlay : public OverlayInstance {
       public:
+
+        explicit ParticleRemoverOverlay(std::shared_ptr<FluidSolver::ParticleRemover3D> entity);
+
+        std::shared_ptr<FluidSolver::ParticleRemover3D> entity = nullptr;
+
         Display get_display() override;
         glm::mat4 get_matrix() override;
         void set_matrix(const glm::mat4& mat_4) override;
@@ -13,4 +19,4 @@ namespace FluidUi {
     };
 
 
-}
+} // namespace FluidUi
