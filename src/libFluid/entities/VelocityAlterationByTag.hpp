7149e742e672079dda60e2ce0391620a75a5cd04
gruco0002 <dev.gruco0002@icloud.com>
2022-09-29 15:18:16 +0200

    Added VelocityAlterationByTag entity

---
+++ src/libFluid/entities/VelocityAlterationByTag.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "entities/SimulationEntity.hpp"
+
+namespace LibFluid {
+    class VelocityAlterationByTag final : public SimulationEntity {
+      public:
+        VelocityAlterationByTag();
+
+        struct VelocityAlterationByTagParameters : public DataChangeStruct {
+            glm::vec3 velocity = glm::vec3(0.0f);
+            pTag_t tag = -1;
+        } parameters;
+
+
+        void execute_simulation_step(const Timepoint& timepoint, bool before_solver) override;
+        void initialize() override;
+        void create_compatibility_report(CompatibilityReport& report) override;
+    };
+} // namespace LibFluid
