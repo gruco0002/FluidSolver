0a9090972e3495004d1145b23769dcfcce2e2430
gruco0002 <dev.gruco0002@icloud.com>
2022-11-03 11:35:44 +0100

    Particle Type can now be changed via tag descriptors

--- src/fluidStudio/userInterface/elements/componentSettings/TagDescriptorSettingsElement.hpp
+++ src/fluidStudio/userInterface/elements/componentSettings/TagDescriptorSettingsElement.hpp
@@ -6,5 +6,10 @@ namespace FluidStudio {
     class TagDescriptorSettingsElement final : public UiElement {
       public:
         void update() override;
+
+      private:
+        LibFluid::ParticleType particle_type = LibFluid::ParticleTypeNormal;
+
+        void apply_type_to_particles(uint32_t particle_tag);
     };
 } // namespace FluidStudio
